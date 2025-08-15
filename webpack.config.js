c561d70591c1abd6e5a53fd0dc8e19bdf2b51003
wellcomemayhem-spec <hirajfaryar@hotmail.com>
2025-08-15 19:55:34 +0800

    Fix regex for CSS file processing in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 // Process CSS files
-                test: /.css$/, 
+                test: /\.css$/,
                 use: ['style-loader', 'css-loader']
             }
         ]
